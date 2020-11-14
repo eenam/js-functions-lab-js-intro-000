b2b8b3d55a15427eaf06694711210a8ae2031ccf
Emma Nam <en2458@gmail.com>
2020-11-14 03:35:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 function happyCustomHolidayTo(holiday, name) {
-
+  return `Happy ${holiday}, ${name}!`;
 }
+
